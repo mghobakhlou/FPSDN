51db9f77d70ebef724e28b37c96789f9a45612c6
mghobakhlou <ghobakhlou.mohammadreza@gmail.com>
2024-07-05 11:18:44 +0330

    Test on other device

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #install all the dependencies
 # sudo apt-get update
-# sudo apt install libncurses5 opam git python3-pip --assume-yes
-# pip3 install numpy networkx
+sudo apt install libncurses5 opam git python3-pip --assume-yes
+pip3 install numpy networkx --break-system-packages
 
 #install NetKAT tool
 # git clone https://github.com/netkat-lang/netkat/
 cd netkat
+eval $(opam env)
 opam init -y
 eval $(opam env)
 opam install mparser=1.2.3 -y
@@ -15,8 +16,8 @@ make
 cd ..
 
 #download Maude
-# wget maude.cs.illinois.edu/w/images/3/38/Maude-3.1-linux.zip
-# unzip Maude-3.1-linux.zip
-# rm Maude-3.1-linux.zip
-# cd maude-3.1
-# chmod +x maude.linux64
+wget maude.cs.illinois.edu/w/images/3/38/Maude-3.1-linux.zip
+unzip Maude-3.1-linux.zip
+rm Maude-3.1-linux.zip
+cd maude-3.1
+chmod +x maude.linux64
