6c7e9a10331690a8db309c7a91fdd60a4f54790a
mghobakhlou <ghobakhlou.mohammadreza@gmail.com>
2024-08-10 23:15:26 +0330

    change fontsize of nodes

--- FPSDN/FPSDN.py
+++ FPSDN/FPSDN.py
@@ -120,18 +120,18 @@ def save_topo_graph(topo, ports,path):
             edge_checker.append(edge)
             edge_checker.append(reverse_edge)
     
-    seed = 65
+    seed = 50
     random.seed(seed)
     pos = nx.spring_layout(G, seed=seed) 
     # pos = nx.spring_layout(G)  # Define the layout for better visualization
 
-    plt.figure(figsize=(12, 8))
+    plt.figure(figsize=(15, 12))
     # Draw the nodes and edges
-    nx.draw(G, pos, node_color=color_map, with_labels=True, node_size=700, font_size=8)
+    nx.draw(G, pos, node_color=color_map, with_labels=True, node_size=700, font_size=16)
 
     
     # Draw edge labels
-    nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8, label_pos=0.5)
+    # nx.draw_networkx_edge_labels(G, pos, edge_labels=edge_labels, font_size=8, label_pos=0.5)
     plt.axis('off')
     plt.savefig(path, format="PNG")
     plt.close()  # Close the figure to free up resources
@@ -512,38 +512,38 @@ def DyNetKAT(topo_graph, packets, expriment_name):
     #                         ]
     #              }
 
-    # # h2h8_h5h7_h1h8_h1h7fault
-    # in_packets = {"h1toh7": "(pt = 5)"}
-    # out_packets = {"h1toh7": "(pt = 20)"}
+    # # h2h8_h5h7_h1h8_h1h7fault --> 2 rcfg
+    in_packets = {"h1toh7": "(pt = 5)"}
+    out_packets = {"h1toh7": "(pt = 20)"}
     
-    # properties = {
-    #               "h1toh7": [
-    #                            ("r", "(head(@Program))", "=0", 2),
-    #                            ("r", "(head(tail(@Program, { rcfg(S53252Reqflow1, \"one\") , rcfg(S53252Upflow1, \"pt = 5 . pt <- 6\") })))", "=0", 3),
-    #                            ("r", "(head(tail(tail(@Program, { rcfg(S53252Reqflow1, \"one\") , rcfg(S53252Upflow1, \"pt = 5 . pt <- 6\") }), { rcfg(S53322Reqflow1, \"one\") , rcfg(S53322Upflow1, \"pt = 12 . pt <- 13\") })))", "=0", 5)
-    #                         ]
-    #              }
+    properties = {
+                  "h1toh7": [
+                               ("r", "(head(@Program))", "=0", 2),
+                               ("r", "(head(tail(@Program, { rcfg(S53252Reqflow1, \"one\") , rcfg(S53252Upflow1, \"pt = 5 . pt <- 6\") })))", "=0", 3),
+                               ("r", "(head(tail(tail(@Program, { rcfg(S53252Reqflow1, \"one\") , rcfg(S53252Upflow1, \"pt = 5 . pt <- 6\") }), { rcfg(S53322Reqflow1, \"one\") , rcfg(S53322Upflow1, \"pt = 12 . pt <- 13\") })))", "=0", 5)
+                            ]
+                 }
     
 
     # h5h7_h1h8_h2h5_h2h8fault --> 3 rcfg
-    in_packets = {"h2toh8": "(pt = 20)"}
-    out_packets = {"h2toh8": "(pt = 18)"}
+    # in_packets = {"h2toh8": "(pt = 20)"}
+    # out_packets = {"h2toh8": "(pt = 18)"}
     
-    properties = {
-                  "h2toh8": [
-                               ("r", "(head(@Program))", "=0", 2),
-                               ("r", "(head(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") })))", "=0", 3),
-                               ("r", "(head(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") })))", "=0", 5),
-                               ("r", "(head(tail(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") }), { rcfg(S44784Reqflow1, \"one\") , rcfg(S44784Upflow1, \"pt = 9 . pt <- 10\") })))", "=0", 5)
-                            ]
-                 }
+    # # properties = {
+    # #               "h2toh8": [
+    # #                            ("r", "(head(@Program))", "=0", 2),
+    # #                            ("r", "(head(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") })))", "=0", 3),
+    # #                            ("r", "(head(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") })))", "=0", 5),
+    # #                            ("r", "(head(tail(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") }), { rcfg(S44784Reqflow1, \"one\") , rcfg(S44784Upflow1, \"pt = 9 . pt <- 10\") })))", "=0", 5)
+    # #                         ]
+    # #              }
     
     
     # properties = {
     #               "h2toh8": [
-    #                            ("r", "(head(@Program))", "=0", 2),
-    #                            ("r", "(head(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") })))", "=0", 3),
-    #                            ("r", "(head(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") })))", "=0", 5)
+    #                         #    ("r", "(head(@Program))", "=0", 2),
+    #                         #    ("r", "(head(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") })))", "=0", 3),
+    #                         #    ("r", "(head(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") })))", "=0", 5)
     #                            ("r", "(head(tail(tail(tail(@Program, { rcfg(S44788Reqflow1, \"one\") , rcfg(S44788Upflow1, \"pt = 15 . pt <- 14\") }), { rcfg(S44718Reqflow1, \"one\") , rcfg(S44718Upflow1, \"pt = 7 . pt <- 6\") }), { rcfg(S44784Reqflow1, \"one\") , rcfg(S44784Upflow1, \"pt = 9 . pt <- 10\") })))", "=0", 7)
     #                         ]
     #              }
@@ -560,8 +560,8 @@ def DyNetKAT(topo_graph, packets, expriment_name):
 if __name__ == "__main__":
 
 
-    expriment_name = "h5h7_h1h8_h2h5_h2h8fault"
-    # expriment_name = "h2h8_h5h7_h1h8_h1h7fault"
+    # expriment_name = "h5h7_h1h8_h2h5_h2h8fault"
+    expriment_name = "h2h8_h5h7_h1h8_h1h7fault"
     # expriment_name = "h2h7_h1h8_h2h8fault"
 
     # expriment_name = "fattree_h2h7_h1h5"
